b649c7d6f2541308be5c38ed83fa17a9724ee8a4
ahmed454090-svg <ahmed454090@gmail.com>
2026-07-18 15:02:38 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then((keys) => {
-        return Promise.all(keys.map(key => caches.delete(key)));
-    }));
-});
-
-// Ye zaroori hai: Request ko default network se aane dein
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
-});
+self.addEventListener('fetch', () => {});
