356e3145b19487b888311bf663b377fad9dc2681
ahmed454090-svg <ahmed454090@gmail.com>
2026-07-18 13:16:56 +0500

    Implement fetch event listener in service worker

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
