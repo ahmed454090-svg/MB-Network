54222d7dd794f75dc50d0abd7f5c9603b81f80b2
ahmed454090-svg <ahmed454090@gmail.com>
2026-07-31 15:08:29 +0500

    Replace webpushr tracking code with importScripts

--- webpushr-sw.js
+++ webpushr-sw.js
@@ -1,5 +1,1 @@
-<!-- start webpushr tracking code --> 
-<script>(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.src = "https://cdn.webpushr.com/app.min.js";
-fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk-alternative'));
-webpushr('init','BLHqPgm19KQDCENTdEiAuLZ-ODYGrKUfEer7lURQilPupBiugLcccC3gUUqB5snL4XIrkbA2RFd7KFuFwdzu5RQ','https://ahmed454090-svg.github.io/MB-Network/webpushr-sw.js','/MB-Network/');</script>
-<!-- end webpushr tracking code -->
+importScripts("https://cdn.webpushr.com/sw-server.min.js");
